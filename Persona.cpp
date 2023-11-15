d3e4c2d8b1cf9aaa39c2e098888d8c9d7235d4b2
Mathias Ahumada <125514738+mathias-ahumada@users.noreply.github.com>
2023-11-15 20:03:30 -0300

    Update Persona.cpp

--- Persona.cpp
+++ Persona.cpp
@@ -1,39 +1,88 @@
-#ifndef PERSONA_H_INCLUDED
-#define PERSONA_H_INCLUDED
+#include "Persona.h"
 
-#include <iostream>
-#include <string>
-#include <limits>
-#include "Fecha.h"
-using namespace std;
-class Persona{
+void Persona::setLegajo(int legajo)
+{
+    _legajo=legajo;
+}
+void Persona::setNombre(string nombre)
+{
+    strcpy(_nombre, nombre.c_str());
+}
 
-protected:
-        int _legajo;
-        char _nombre[30];
-        char _apellido[30];
-        class Fecha _fechaDeAlta;
-        float _totalVendido;
-        bool _estado;
+void Persona::setApellido(string apellido)
+{
+    strcpy(_apellido, apellido.c_str());
+}
 
-public:
+void Persona::setFechaDeAlta(Fecha fechaDeAlta)
+{
+    _fechaDeAlta.cargar();
+}
 
-        void setLegajo(int legajo);
-        void setNombre(string nombre);
-        void setApellido(string apellido);
-        void setFechaDeAlta(class Fecha fechaDeAlta);
-        void setEstado(bool estado);
+void Persona::setEstado(bool estado)
+{
+    _estado=estado;
+}
 
-        int getLegajo() const;
-        string getNombre() const;
-        string getApellido() const;
-        class Fecha getFechaDeAlta() const;
-        bool getEstado() const;
+int Persona::getLegajo() const
+{
+    return _legajo;
+}
 
-        void cargar(int f);
-         void mostrar();
+string Persona::getNombre() const
+{
+    return _nombre;
+}
+
+string Persona::getApellido() const
+{
+    return _apellido;
+}
+
+Fecha Persona::getFechaDeAlta() const
+{
+    return Fecha(_fechaDeAlta);
+}
+
+bool Persona::getEstado() const
+{
+    return _estado;
+}
+
+void Persona::cargar(int f)
+{
+    _estado=true;
 
 
-};
+    while (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout<<"Valor invalido, ingrese solo numeros"<<endl;
+            cout<<"Ingrese nuevamente el valor"<<endl;
+            _legajo=f;
 
-#endif // PERSONA_H_INCLUDED
+        }
+    cout<<"Ingrese nombre: "<<endl;
+    cin.ignore();
+    cin.getline(_nombre, 30);
+    cout<<"Ingrese apellido: "<<endl;
+    cin.getline(_apellido, 30);
+    cout<<"Ingrese fecha de Alta de forma ordenada"<<endl;
+    _fechaDeAlta.cargar();
+    while(_fechaDeAlta.getAnio()==0){
+            cout<<"La fecha ingresada no existe en el calendario, ingrese una fecha valida"<<endl;
+            _fechaDeAlta.cargar();
+        }
+
+
+}
+
+void Persona::mostrar()
+{
+
+    cout<<"Legajo: "<<_legajo<<endl;
+    cout<<"Nombre y apellido "<<_nombre<<" "<<_apellido<<endl;
+    cout<<"Fecha de alta"<<_fechaDeAlta.toString()<<endl;
+
+}
